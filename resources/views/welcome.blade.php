04a462114a9eda8041985ea0b937bd5bcb33661c
akosi1 <bunakidboi@gmail.com>
2025-09-26 18:13:05 +0800

    c

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MCC Event & Portfolio Organizer</title>
 
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('favicon.ico') }}?v={{ filemtime(public_path('favicon.ico')) }}" type="image/x-icon">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700&display=swap" rel="stylesheet" />
@@ -132,6 +135,5 @@
             console.warn('Google reCAPTCHA site key is not configured.');
         </script>
     @endif
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 </body>
 </html>
