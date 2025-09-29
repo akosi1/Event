0972ed34072ac905b67402b940c56ed366918c1c
akosi1 <bunakidboi@gmail.com>
2025-09-30 02:02:49 +0800

    a

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,4 @@
 <x-guest-layout>
- <link rel="icon" href="{{ asset('favicon.ico') }}?v=2" type="image/x-icon">
     <div class="auth-wrapper">
         <div class="auth-container">
             <div class="auth-header">
