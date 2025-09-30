b7559a7bc97740c923ae69b6d2cc4f63e6dd677b
akosi1 <bunakidboi@gmail.com>
2025-09-30 14:20:52 +0800

    change

--- config/mail.php
+++ config/mail.php
@@ -36,18 +36,23 @@ return [
     */
 
     'mailers' => [
+        
 
         'smtp' => [
             'transport' => 'smtp',
             'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '127.0.0.1'),
+            'host' => env('MAIL_HOST', 'smpt.gmail.com'),
             'port' => env('MAIL_PORT', 2525),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            // 'log' => [
+            //     'channel' => 'stderr',
+            ]
         ],
+
 
         'ses' => [
             'transport' => 'ses',
@@ -111,8 +116,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'briannickacorda18@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'EventAps - McLawis College'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
 ];
