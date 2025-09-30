076f31fd158f13d5d8d10311acef9e395cd5259b
akosi1 <bunakidboi@gmail.com>
2025-09-30 13:22:02 +0800

    change

--- app/Http/Controllers/Auth/MS365OTPController.php
+++ app/Http/Controllers/Auth/MS365OTPController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\OtpVerification;
-use Illuminate\Http\Request;ka lag
+use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Mail;
