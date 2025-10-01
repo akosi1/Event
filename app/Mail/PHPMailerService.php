616e7b9b27e237f2ffe18f14faacb0fa908176f6
akosi1 <bunakidboi@gmail.com>
2025-10-01 23:43:56 +0800

    chnage

--- app/Mail/PHPMailerService.php
+++ app/Mail/PHPMailerService.php
@@ -4,6 +4,7 @@ namespace App\Mail;
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use Illuminate\Support\Facades\Log;
 
 class PHPMailerService
 {
@@ -12,36 +13,43 @@ class PHPMailerService
         $mail = new PHPMailer(true);
 
         try {
-            $mail->SMTPDebug = 0;
+          
+            $mail->SMTPDebug = 2;
+            $mail->Debugoutput = function ($str, $level) {
+                Log::info("PHPMailer debug level {$level}: {$str}");
+            };
 
+            
             $mail->isSMTP();
             $mail->Host = env('MAIL_HOST', 'smtp.gmail.com');
             $mail->SMTPAuth = true;
-            $mail->Username = env('MAIL_USERNAME');
-            $mail->Password = env('MAIL_PASSWORD');
+            $mail->Username = env('MAIL_USERNAME'); 
+            $mail->Password = env('MAIL_PASSWORD'); 
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = env('MAIL_PORT', 587);
 
-            $mail->CharSet = 'UTF-8';
-
+         
             $fromEmail = env('MAIL_FROM_ADDRESS', env('MAIL_USERNAME'));
             $fromName = env('MAIL_FROM_NAME', 'EventAps');
 
             $mail->setFrom($fromEmail, $fromName);
-            $mail->addReplyTo($fromEmail, $fromName);
 
+           
             $mail->addAddress($toEmail);
 
+           
             $mail->isHTML(true);
             $mail->Subject = $subject;
             $mail->Body = $body;
-            $mail->AltBody = strip_tags($body);
 
             $mail->send();
 
+            Log::info("PHPMailer: Email sent to {$toEmail} with subject: {$subject}");
             return true;
 
         } catch (Exception $e) {
+            Log::error("PHPMailer Error sending to {$toEmail}: " . $mail->ErrorInfo);
+            Log::error("Exception: " . $e->getMessage());
             return false;
         }
     }
