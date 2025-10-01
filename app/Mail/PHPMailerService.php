5c5629fb556a2528571d010b2474e826a13bceae
akosi1 <bunakidboi@gmail.com>
2025-10-01 22:06:24 +0800

    change

--- app/Mail/PHPMailerService.php
+++ app/Mail/PHPMailerService.php
@@ -13,13 +13,11 @@ class PHPMailerService
         $mail = new PHPMailer(true);
 
         try {
-          
             $mail->SMTPDebug = 2;
             $mail->Debugoutput = function ($str, $level) {
                 Log::info("PHPMailer debug level {$level}: {$str}");
             };
 
-            
             $mail->isSMTP();
             $mail->Host = env('MAIL_HOST', 'smtp.gmail.com');
             $mail->SMTPAuth = true;
@@ -28,19 +26,21 @@ class PHPMailerService
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = env('MAIL_PORT', 587);
 
-         
+            
             $fromEmail = env('MAIL_FROM_ADDRESS', env('MAIL_USERNAME'));
             $fromName = env('MAIL_FROM_NAME', 'EventAps');
 
             $mail->setFrom($fromEmail, $fromName);
 
-           
             $mail->addAddress($toEmail);
 
-           
             $mail->isHTML(true);
             $mail->Subject = $subject;
             $mail->Body = $body;
+
+            // Add a unique Message-ID here
+            $domain = parse_url(env('APP_URL'), PHP_URL_HOST) ?: 'mcclawis.edu.ph';
+            $mail->MessageID = sprintf('<%s@%s>', md5(uniqid(time(), true)), $domain);
 
             $mail->send();
 
