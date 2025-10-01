be142f42d7e02f089b91db0769ab6250bb374746
akosi1 <bunakidboi@gmail.com>
2025-10-01 19:49:19 +0800

    change

--- config/mail.php
+++ config/mail.php
@@ -41,8 +41,7 @@ return [
             'transport' => 'smtp',
             'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '
-            '),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 2525),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
