45567f40df1557b8052e25d2332661669a88ca71
akosi1 <bunakidboi@gmail.com>
2025-10-08 05:41:27 +0800

    try

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
     
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <!-- <link rel="stylesheet" href="{{ asset('user/welcome/welcome.css') }}"> -->
+    <link rel="stylesheet" href="{{ asset('user/welcome/welcome.css') }}">
     <style>
               body {
             background: url("{{ asset('images/mcc background.jpg') }}") center/cover no-repeat fixed;
