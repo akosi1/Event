569857c653c983aa2c7e4e938185a9f8012b320b
akosi1 <bunakidboi@gmail.com>
2025-10-10 18:58:53 +0800

    s

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -13,10 +13,13 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-
+        \App\Http\Middleware\ContentSecurityPolicy::class,
         // ✅ Security headers applied to every request
         \App\Http\Middleware\SecurityHeaders::class,
     ];
+
+    // ...
+    
 
     protected $middlewareGroups = [
         'web' => [
