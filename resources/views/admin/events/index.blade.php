0e9b6b47d8e43bbf2c58fc363e122e1f87bb54b7
akosi1 <bunakidboi@gmail.com>
2025-10-18 20:28:40 +0800

    Change

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -126,8 +126,8 @@
                             </td>
                             <td class="py-2 align-middle">
                                 <div class="event-image-compact">
-                                    <!-- @if($event->image && file_exists(public_path('storage/' . $event->image))) -->
-                                     <img src="{{ $event->image_url }}" alt="{{ $event->title }}" class="event-img-compact">
+                                    @if($event->image && file_exists(public_path('storage/' . $event->image)))
+                                     <!-- <img src="{{ $event->image_url }}" alt="{{ $event->title }}" class="event-img-compact"> -->
                                         <img src="{{ asset('storage/' . $event->image) }}" 
                                              alt="{{ $event->title }}" 
                                              class="event-img-compact"
