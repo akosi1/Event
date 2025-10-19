21a7e4f66d221046cb2ff91a937eeb58f06344d5
akosi1 <bunakidboi@gmail.com>
2025-10-20 01:17:44 +0800

    chanhge

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -59,7 +59,7 @@ class Event extends Model
 
     public function hasImage(): bool
     {
-        return !empty($this->image) && file_exists(public_path('public/' . $this->image));
+        return !empty($this->image) && file_exists(public_path('public/storage/' . $this->image));
     }
 
     public function getImageUrlAttribute(): string
@@ -77,8 +77,8 @@ class Event extends Model
             return asset('images/default-event.jpg');
         }
 
-        if (file_exists(public_path('public/' . $this->image))) {
-            return asset('public/' . $this->image);
+        if (file_exists(public_path('public/storage' . $this->image))) {
+            return asset('public/storage' . $this->image);
         }
 
         return asset('images/default-event.jpg');
@@ -87,7 +87,7 @@ class Event extends Model
     public function getImagePath(): string
     {
         if ($this->hasImage()) {
-            return asset('public/' . $this->image);
+            return asset('public/storage' . $this->image);
         }
 
         return asset('images/default-event.jpg');
@@ -95,7 +95,7 @@ class Event extends Model
 
     public function deleteImage(): bool
     {
-        $path = public_path('public/' . $this->image);
+        $path = public_path('public/storage' . $this->image);
         if ($this->image && file_exists($path)) {
             try {
                 unlink($path);
