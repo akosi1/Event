d48f9dc5fe9f04ae2bcbc0f872efaa9f1e8046a6
akosi1 <bunakidboi@gmail.com>
2025-10-21 09:29:31 +0800

    feedback add email

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,6 @@
             Route::post('{event}/join', [EventJoinController::class, 'join'])->name('join');
             Route::delete('{event}/leave', [EventJoinController::class, 'leave'])->name('leave');
             Route::post('{id}/feedback', [EventController::class, 'storeFeedback'])->name('feedback');
-            Route::post('{event}/feedback', [EventController::class, 'storeFeedback'])->name('feedback');
         });
     });
 
