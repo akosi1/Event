b5b7dd788ff702b9603a7524d9c359cf2cd99973
akosi1 <bunakidboi@gmail.com>
2025-10-21 09:56:46 +0800

    feedback add email

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,8 @@
     <title>{{ config('app.name', 'EventAP') }} - Dashboard</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link href="{{ asset('user/dashboard/dashboard.css') }}" rel="stylesheet">
-    <link href="{{ asset('user/nav/css/navbar.css') }}" rel="stylesheet">
-    <link href="{{ asset('user/footer/footer.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('user/nav/css/navbar.css') }}" rel="stylesheet">
+    <link href="{{ asset('user/footer/footer.css') }}" rel="stylesheet"> --}}
     <!-- SweetAlert2 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.min.css" rel="stylesheet">
 
