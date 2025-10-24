23e3e19be6a3019d3c60ba837a829c24bb1cf12d
akosi1 <bunakidboi@gmail.com>
2025-10-24 13:00:20 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -10,16 +10,11 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
-<<<<<<< HEAD
 //test
 require __DIR__.'/../vendor/autoload.php';
-=======
-// Register the Composer autoloader...
-require __DIR__.'/vendor/autoload.php';
->>>>>>> f196b26de65e9611396b3fc01df3b9f4cfe1758e
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
