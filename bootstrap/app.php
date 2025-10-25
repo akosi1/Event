645c4cf60cd077bcb13c7554ba69702e034847ca
akosi1 <bunakidboi@gmail.com>
2025-10-25 14:38:08 +0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ use App\Http\Middleware\IsAdmin;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
+        commands: __DIR__ . '/routes/console.php',
         health: '/up',
 
         // Optional: Register additional route files
