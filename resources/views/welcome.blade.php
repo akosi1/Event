8ac0dc4563e36f60f40f71a5708e4ba95fb16b4d
akosi1 <bunakidboi@gmail.com>
2025-10-26 19:51:22 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MCC Event & Portfolio Organizer</title>
-     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link href="{{ asset('user/footer/footer.css') }}" rel="stylesheet">    
+    # <link rel="stylesheet" href="{{ asset('css/style.css') }}"/>
+  #  <link href="{{ asset('user/footer/footer.css') }}" rel="stylesheet"/>    
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
