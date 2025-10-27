e654e2680f357c5e4d6da36f4ab59bed11af5181
Dev Son <devson@example.com>
2025-10-27 09:20:03 +0800

    remake

--- resources/views/layouts/intronav.blade.php
+++ resources/views/layouts/intronav.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar" id="navbar">
     <div class="nav-container">
         <a href="#home" class="nav-logo">
-            <img src="{{ asset('public/images/logo.png') }}" alt="MCC Logo">
+            <img src="{{ asset('images/logo.png') }}" alt="MCC Logo">
             <span>MCC E&PO</span>
         </a>
         <ul class="nav-menu" id="navMenu">
